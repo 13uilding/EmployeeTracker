86cd02b3fde45c76f4bc2ea75a2350ff709dc214
deep <hereisdanbrown@gmail.com>
2020-01-28 22:07:27 -0700

    Got the remove functionality working on departments again. Just need to add the view and I'm back to where I was yesterday, but we better refactored code

--- employeeTracker.js
+++ employeeTracker.js
@@ -16,9 +16,8 @@ var connection = mysql.createConnection({
 })
 
 
-connection.connect(async function(err) {
-    if (err) throw err;
-    var [departments, roles, employees] = await getTables(); 
+connection.connect(function(err) {
+    if (err) throw err; 
     // console.log(roles);
     doWhat();
 })
@@ -39,7 +38,9 @@ const testingDep = doQs.splice(12);
 
 
     // What would you like to do Questionnaire
-function doWhat() {
+async function doWhat() {
+    try{
+    var [departments, roles, employees] = await getTables();
     inquirer.prompt({
         name: "do",
         message: "What would you like to do?",
@@ -68,7 +69,14 @@ function doWhat() {
                 doWhat();
                 break;
             case "Remove Department":
-                department.removeMySQL(doWhat);
+                inquirer.prompt({
+                    name: "remove",
+                    message: "Which department would you like to remove?",
+                    type: "list",
+                    choices: choicesArr("name", database, departments)
+                }).then(function(answer) {
+                    elementRemove(answer, database, departments);
+                });
                 break;
             case "View All Employees":
                 break;
@@ -89,12 +97,12 @@ function doWhat() {
                     name: "role",
                     message: "What is their role?",
                     type: "list",
-                    choices: choicesArr(roles, "name")
+                    choices: choicesArr("name", database, roles)
                 }, {
                     name: "manager",
                     message: "Who is their manager?",
                     type: "list",
-                    choices: choicesArr(employee, "name")
+                    choices: choicesArr("name", database, employees)
                 }]).then(function(answers) {
                     elementAdd(answers, database)
                 });
@@ -122,6 +130,8 @@ function doWhat() {
         }
 
     });
+            
+} catch {err}
 };
 
 async function init() {
@@ -136,11 +146,31 @@ function elementAdd(obj, database) {
         const query = `INSERT INTO ${database} SET ?`
         connection.query(query, obj, function(err, res) {
             if (err) throw err;
-            doWhat();
+            return doWhat();
         })
         
     })
 }
+
+function elementRemove(obj, database, databaseArr) {
+    if (obj.remove === "None") {
+        doWhat();
+    };
+    if (database === "departments") {
+        var match = databaseArr.find(department => obj.remove === department.name)
+    } else if (database === "roles") {
+        var match = databaseArr.find(role => obj.remove === role.title)
+    } else {
+        var match = databaseArr.find(employee => obj.remove === `${employee.first_name} ${employee.last_name}`)
+    }
+        let id = match.id;
+        console.log(id);
+        connection.query(`DELETE FROM ${database} WHERE ?`, {id: id}, function(err, res) {
+            if (err) throw err;
+            return doWhat();
+        })     
+}
+
 // Gets fresh tables
 function getTables() {
     return new Promise((resolve, reject) => {
@@ -156,11 +186,11 @@ function getTables() {
 }
 
 // Generates choices array for remove and ids (not great for manager)
-function choicesArr(database, type) {
+function choicesArr(type, database, databaseArr) {
     var choices = [];
     switch (database) {
         case "departments":
-            for (const department of departments) {
+            for (const department of databaseArr) {
                 if (type === "name") {
                     choices.push((department.name).toLowerCase());
                 } else {
@@ -169,7 +199,7 @@ function choicesArr(database, type) {
             };
             break;
         case "roles":
-            for (const role of roles) {
+            for (const role of databaseArr) {
                 if (type === "name") {
                     choices.push((role.title).toLowerCase());
                 } else if (type === "id") {
@@ -180,7 +210,7 @@ function choicesArr(database, type) {
             };
             break;
         case "employees":
-            for (const employee of employees) {
+            for (const employee of databaseArr) {
                 if (type === "name") {
                     let pushy = (employee.first_name).toLowerCase()
                     pushy += " " + (employee.last_name).toLowerCase()
